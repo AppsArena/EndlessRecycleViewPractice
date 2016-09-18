1c35031c63b6ed34e5ea35d1c158b3e4dd033ef7
mitu <catvinchy@gmail.com>
2016-09-18 16:58:13 +0600

    edited

--- app/src/main/java/example/com/endlessrecycleviewpracticez/UserPanelApiInterface.java
+++ app/src/main/java/example/com/endlessrecycleviewpracticez/UserPanelApiInterface.java
@@ -1,20 +1,12 @@
 package example.com.endlessrecycleviewpracticez;
 
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.http.Body;
-import retrofit2.http.GET;
-import retrofit2.http.POST;
-import retrofit2.http.Path;
-
 /**
  * Created by mitu on 9/18/16.
  */
 public interface UserPanelApiInterface {
-    @POST("/CustomerInfo/OrderList/{customerId}/{status}")
+   /* @POST("/CustomerInfo/OrderList/{customerId}/{status}")
     Call<List<CustomerInfoOrderListModel>> getCustomerInfo(@Path("customerId") int customerId, @Path("status") int status, @Body CustomerOrderListLimitModel customerOrderListLimitModel);
     @GET("/CustomerInfo/OrderListCount/{customerId}/{status}")
-    Call<CountModel> getTotalCountofOrderlist(@Path("customerId") int customerId,@Path("status") int status);
+    Call<CountModel> getTotalCountofOrderlist(@Path("customerId") int customerId,@Path("status") int status);*/
 
 }
